b639c81e27ae5ea40fd05ef9ff584ad039f10a70
Josh <josh@halfnibble.com>
2017-03-22 16:25:25 -0700

    Configure deployment on heroku.

--- app.py
+++ app.py
@@ -39,3 +39,8 @@ def madlibstory():
     }
     return render_template('story.html', context=context)
 
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 8080))
+    host = os.getenv('IP', '0.0.0.0')
+    app.run(port=port, host=host)
+
